6152d8c64956b30fff023401e64bd0f5a95a71ed
Anes Abdelrahman <anes.abdelrahman@findel-education.co.uk>
2019-11-28 09:16:52 +0000

    PigIt implementation

---
+++ CodingKata/CodeWars/PigItTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class PigItTests
+    {
+        private PigItKata _pigItKata;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pigItKata = new PigItKata();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _pigItKata = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", _pigItKata.PigIt2("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", _pigItKata.PigIt2("This is my string"));
+
+        }
+    }
+
+    public class PigItKata
+    {
+        // Mine
+        public string PigIt(string str)
+        {
+            return string.Join(' ', str.Split(' ').Select(x => new string(x.Append(x.ElementAt(0)).Skip(1).Concat("ay").ToArray())));
+        }
+
+        //Best practice - other
+        public string PigIt2(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(w => w.Substring(1) + w[0] + "ay"));
+        }
+    }
+}
+
+    
