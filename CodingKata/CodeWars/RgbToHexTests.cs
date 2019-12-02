a2183f8d3543d3588ce72bfc7633ec5c92cac527
Anes Abdelrahman <anes.abdelrahman@findel-education.co.uk>
2019-12-02 13:48:50 +0000

    WhichAreIn and RgbToHex

---
+++ CodingKata/CodeWars/RgbToHexTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class RgbToHexTests
+    {
+        private RgbToHex _rgbToHex;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rgbToHex = new RgbToHex();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _rgbToHex = null;
+        }
+
+        [Test]
+        [TestCase(255,255,255, "FFFFFF")]
+        [TestCase(255, 255, 300, "FFFFFF")]
+        [TestCase(0,0,0, "000000")]
+        [TestCase(148, 0, 211, "9400D3")]
+        [TestCase(148, -20, 211, "9400D3")]
+        [TestCase(144, 195, 212, "90C3D4")]
+        [TestCase(212, 53, 12, "D4350C")]
+        public void Test1(int r, int g, int b, string hex)
+        {
+            Assert.AreEqual(hex, _rgbToHex.Rgb2(r,g,b));
+        }
+    }
+
+    public class RgbToHex
+    {
+        // Mine
+        public string Rgb(int r, int g, int b)
+        {
+            var numbers = new List<int>{r,g,b};
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers[i] = 0;
+                    continue;
+                }
+
+                if (numbers[i] > 255)
+                {
+                    numbers[i] = 255;
+                }
+            }
+
+            return $"{numbers[0]:X2}{numbers[1]:X2}{numbers[2]:X2}";
+        }
+
+        public string Rgb2(int r, int g, int b)
+        {
+           return $"{Math.Clamp(r, 0, 255):X2}{Math.Clamp(g, 0, 255):X2}{Math.Clamp(b, 0, 255):X2}";
+        }
+    }
+}
+
+    
---
+++ CodingKata/CodeWars/WhichAreInTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class WhichAreInTests
+    {
+        private WhichAreIn _whichAreIn;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _whichAreIn = new WhichAreIn();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _whichAreIn = null;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var a1 = new[] { "arp", "live", "strong" };
+            var a2 = new[] { "lively", "alive", "harp", "sharp", "armstrong" };
+            var r = new[] { "arp", "live", "strong" };
+
+            var a3 = new[] { "tarp", "mice", "bull" };
+            var a4 = new[] { "lively", "alive", "harp", "sharp", "armstrong" };
+            var r2 = new string[0];
+            Assert.AreEqual(r, WhichAreIn.InArray(a1, a2));
+            Assert.AreEqual(r2, WhichAreIn.InArray(a3, a4));
+        }
+    }
+
+    public class WhichAreIn
+    {
+        // Mine
+        public static string[] InArray(string[] array1, string[] array2)
+        {
+            return array1.Where(x => array2.Any(y => y.Contains(x))).Distinct().OrderBy(y => y).ToArray(); 
+        }
+    }
+}
+
+    
