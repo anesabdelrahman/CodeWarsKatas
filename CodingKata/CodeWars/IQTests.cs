3f748a9629c78cd419e71b0af9d8610a8c479e3b
Anes Abdelrahman <anes.abdelrahman@findel-education.co.uk>
2019-12-05 08:40:53 +0000

    IQ and Narcissistic tests

---
+++ CodingKata/CodeWars/IQTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class IQTests
+    {
+        private IQ _iq;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _iq = new IQ();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _iq = null;
+        }
+
+        [Test]
+        [TestCase("2 4 7 8 10", 3)]
+        [TestCase("1 2 2", 1)]
+        public void Test1(string input, int expected)
+        {
+            Assert.AreEqual(expected, _iq.Test(input));
+        }
+    }
+
+    public class IQ
+    {
+        public int Test(string numbers)
+        {
+            var numberList = new List<int>(numbers.Split(' ').Select(x => Convert.ToInt32(x.ToString())));
+            var evenNumbers = numberList.Where(y => y > 0 && y % 2 == 0).ToList();
+            return numberList.IndexOf(evenNumbers.Count() > numberList.Except(evenNumbers).Count() ? numberList.Except(evenNumbers).FirstOrDefault() : evenNumbers.FirstOrDefault()) + 1;
+        }
+    }
+}
+
+    
---
+++ CodingKata/CodeWars/NarcissisticTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class NarcissisticTests
+    {
+        private NarcissisticClass _narcissisticClass;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _narcissisticClass = new NarcissisticClass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _narcissisticClass = null;
+        }
+
+        [Test]
+        [TestCase(1, true)]
+        [TestCase(153, true)]
+        [TestCase(371, true)]
+        [TestCase(1634, true)]
+        [TestCase(1635, false)]
+        public void Test1(int input, bool expected)
+        {
+            Assert.AreEqual(expected, _narcissisticClass.Narcissistic(input));
+        }
+    }
+
+    public class NarcissisticClass
+    {
+        // Mine
+        public bool Narcissistic(int value)
+        {
+            return value == (int)value.ToString().Select(x => Math.Pow(Convert.ToInt32(x.ToString()), value.ToString().Length)).Sum(); 
+        }
+    }
+}
+
+    
