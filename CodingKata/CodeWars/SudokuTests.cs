25a7ea2df7c146fc4b21e428bebc6abef154ce9d
Anes Abdelrahman <anes.abdelrahman@findel-education.co.uk>
2019-12-09 14:13:03 +0000

    Adding Seduko kata

---
+++ CodingKata/CodeWars/SudokuTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace CodingKata.CodeWars
+{
+    public class SudokuTests
+    {
+        private Sudoku _Sudoku;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Sudoku = new Sudoku();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _Sudoku = null;
+        }
+
+        private static object[] _testCases = {
+            new object[]
+            {
+                true,
+                new int[][]
+                {
+                    new int[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                    new int[] {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                    new int[] {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                    new int[] {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                    new int[] {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                    new int[] {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                    new int[] {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                    new int[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                    new int[] {3, 4, 5, 2, 8, 6, 1, 7, 9}
+                }
+            },
+            new object[]
+            {
+                false,
+                new int[][]
+                {
+                    new int[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                    new int[] {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                    new int[] {1, 9, 8, 3, 0, 2, 5, 6, 7},
+                    new int[] {8, 5, 0, 7, 6, 1, 4, 2, 3},
+                    new int[] {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                    new int[] {7, 0, 3, 9, 2, 4, 8, 5, 6},
+                    new int[] {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                    new int[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                    new int[] {3, 0, 0, 2, 8, 6, 1, 7, 9}
+                }
+            },
+            new object[]
+            {
+                false,
+                new int[][]
+                {
+                    new int[] {1, 2, 6, 3, 4, 7, 5, 9, 8},
+                    new int[] {7, 3, 5, 8, 1, 9, 6, 4, 2},
+                    new int[] {1, 9, 4, 2, 6, 5, 8, 7, 3},
+                    new int[] {3, 1, 7, 5, 8, 4, 2, 6, 9},
+                    new int[] {6, 5, 9, 1, 7, 2, 4, 3, 8},
+                    new int[] {4, 8, 2, 9, 3, 6, 7, 1, 5},
+                    new int[] {9, 4, 8, 7, 5, 1, 3, 2, 6},
+                    new int[] {5, 6, 1, 4, 2, 3, 9, 8, 7},
+                    new int[] {2, 7, 3, 6, 9, 8, 1, 5, 4}
+                }
+            },
+            new object[]
+            {
+                false,
+                new int[][]
+                {
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {2, 3, 1, 5, 6, 4, 8, 9, 7},
+                    new int[] {3, 1, 2, 6, 4, 5, 9, 7, 8},
+                    new int[] {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                    new int[] {5, 6, 4, 8, 9, 7, 2, 3, 1},
+                    new int[] {6, 4, 5, 9, 7, 8, 3, 1, 2},
+                    new int[] {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                    new int[] {8, 9, 7, 2, 3, 1, 5, 6, 4},
+                    new int[] {9, 7, 8, 3, 1, 2, 6, 4, 5}
+                }
+            },
+            new object[]
+            {
+                false,
+                new int[][]
+                {
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                    new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}
+                }
+            }
+        };
+
+        [Test]
+        [TestCaseSource(nameof(_testCases))]
+        public void Test1(bool expected, int[][] input)
+        {
+            Assert.AreEqual(expected, _Sudoku.ValidateSolution(input));
+        }
+    }
+
+    public class Sudoku
+    {
+        public bool ValidateSolution(int[][] board)
+        {
+            var standard = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var firstBlock = board.Take(3).ToList();
+            var secondBlock = board.Skip(3).Take(3).ToList();
+            var thirdBlock = board.Skip(6).Take(3).ToList();
+
+            foreach (var single in board)
+            {
+                var block = new List<int>();
+                block.AddRange(firstBlock[0].Take(3));
+                block.AddRange(firstBlock[1].Take(3));
+                block.AddRange(firstBlock[2].Take(3));
+
+
+                block = block.OrderBy(x => x).ToList();
+                if (!block.SequenceEqual(standard))
+                {
+                    return false;
+                }
+
+                block = new List<int>();
+                block.AddRange(secondBlock[0].Take(3));
+                block.AddRange(secondBlock[1].Take(3));
+                block.AddRange(secondBlock[2].Take(3));
+
+
+                block = block.OrderBy(x => x).ToList();
+                if (!block.SequenceEqual(standard))
+                {
+                    return false;
+                }
+
+                block = new List<int>();
+                block.AddRange(thirdBlock[0].Take(3));
+                block.AddRange(thirdBlock[1].Take(3));
+                block.AddRange(thirdBlock[2].Take(3));
+
+
+                block = block.OrderBy(x => x).ToList();
+                if (!block.SequenceEqual(standard))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+    
